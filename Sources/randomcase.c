c5e9478aa0fbfc0f05f64afd7c465572bcbb88ef
SBAPKat <sbapkat@gmail.com>
2020-06-25 23:00:25 +0200

    Removed unused var, improved comments

--- Sources/randomcase.c
+++ Sources/randomcase.c
@@ -3,23 +3,23 @@
 #include <stdio.h> //debug
 
 void GenCase(int** GameMatrix){
-	int random, randomranged, RandA, RandB, i;
-	srand(time(0)); //curent time as rseed
+	int random, randomranged, RandA, RandB;
+	srand(time(0)); //current time as rseed
 	random = (rand()%(3/*high*/ - 0/*lower*/ + 1))+0/*lower*/; //generates a value between 0 and 3
 	do{
 		RandA=(rand()%(3/*high*/ - 0/*lower*/ + 1))+0/*lower*/; //generates a value between 0 and 3 
 		RandB=(rand()%(3/*high*/ - 0/*lower*/ + 1))+0/*lower*/; //generates a value between 0 and 3 
 		printf("GameMatrix[%i][%i]", RandA, RandB);		//debug
 	}
-	while(GameMatrix[RandA][RandB]!=0);
+	while(GameMatrix[RandA][RandB]!=0); //As long as the selected slot isnt empty, select another one
 	if(random==0){
 		//gen a 4
-		printf("%i\n", 4);
-		Gamematrix[RandA][RandB] = 4;
+		printf("%i\n", 4);	//debug
+		Gamematrix[RandA][RandB] = 4; //Write to slot
 	}
 	else{
 		//gen a 2
-		printf("%i\n", 2);
-		Gamematrix[RandA][RandB] = 2;
+		printf("%i\n", 2);	//debug
+		Gamematrix[RandA][RandB] = 2; //Write to slot
 	}
 }
