7a41ce72283cb76b82292c740a5e5e93907aea1e
SBAPKat <sbapkat@gmail.com>
2020-06-27 00:27:25 +0200

    now have infinite display to debug

--- Sources/Gamelogic.c
+++ Sources/Gamelogic.c
@@ -2,12 +2,14 @@
 
 
 void printMatrix (int lineNumber, int columnNumber, int** matrix){
+    printf("_________________\n");
 	for (int i=0;i<lineNumber;i++){
 		for (int j=0;j<columnNumber;j++){
 			printf(" %d |",matrix[i][j]);
 		}
 		printf("\n");
 	}
+    printf("_________________\n");
 	return;
 }
 
--- Sources/main.c
+++ Sources/main.c
@@ -5,11 +5,14 @@
 
 int main(int argc, char** argv){
 	int** GameMatrix = generateMatrix(4,4);
+    char *unused;
+    unused = NULL;
 	printMatrix (4,4,GameMatrix);
     GenCase (GameMatrix);	
-	printMatrix (4,4,GameMatrix);
-    GenCase (GameMatrix);	
-	printMatrix (4,4,GameMatrix);
-    GenCase (GameMatrix);	
+    while(1){
+        printMatrix (4,4,GameMatrix);
+        GenCase (GameMatrix);
+        scanf("%c",unused);
+    }	
 	return EXIT_SUCCESS;
-	}
+}
