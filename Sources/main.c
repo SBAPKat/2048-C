3829db35be6e274cb1ac4590142f561504e1df6f
SBAPKat <sbapkat@gmail.com>
2020-06-30 17:25:12 +0200

    added support for basic user input

--- Sources/main.c
+++ Sources/main.c
@@ -5,14 +5,18 @@
 
 int main(int argc, char** argv){
 	int** GameMatrix = generateMatrix(4,4);
-    char *unused;
-    unused = NULL;
+    unsigned char input;
 	printMatrix (4,4,GameMatrix);
     GenCase (GameMatrix);	
     while(1){
-        printMatrix (4,4,GameMatrix);
-        GenCase (GameMatrix);
-        scanf("%c",unused);
+        input=fgetc(stdin);
+        if(input=='a'){
+            GenCase (GameMatrix);
+            printMatrix (4,4,GameMatrix);
+        }
+        else{
+            if(input!=EOF) printf("sudo press a\n"); //does not work for some reason, still displays press a everytime
+        }
     }	
 	return EXIT_SUCCESS;
 }
