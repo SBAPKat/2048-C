0366bbde9842720c8e97439bc372514136dc19f6
SBAPKat <sbapkat@gmail.com>
2020-07-14 22:05:33 +0200

    added TODO for OPENGL

--- Sources/main.c
+++ Sources/main.c
@@ -3,19 +3,26 @@
 #include "Gamelogic.h"
 #include "check_args.h"
 #include "CMD.h"
+#include <stdio.h>
 
 int main(int argc, char** argv){
 	int** GameMatrix = generateMatrix(4,4);	//Creates the game matrix
-    int option;								//Variable in which the option will be stored
-    option = check_args (argc, argv);		//Checks the argument to choose a display method
-    if(option==1) {							//If there are no arguments, ncurses is selected
-        while(mainCMD (GameMatrix)){		//Checks if you still want to play or if you quit
-            GameMatrix=generateMatrix(4,4);	//If you still want to play, reset game matrix
-        }
-    }
-    else{
-        if(option==2) {/*SDL*/}
-        else printf("Bad flag");
-    }
-   	return EXIT_SUCCESS;
+	switch (check_args(argc,argv[1])) {
+		case 1:
+			while(mainCMD (GameMatrix)){		//Checks if you still want to play or if you quit
+				GameMatrix=generateMatrix(4,4);	//If you still want to play, reset game matrix
+			}
+			break;
+
+		case 2:
+			//TODO:IMPLEMENT SDL
+			printf("SDL SELECTED");
+			break;
+		
+		case 3:
+			//TODO:IMPLEMENT OPENGL
+			printf("OPENGL SELECTED");
+			break;
+	}
+	return EXIT_SUCCESS;
 }
