67bc0156cb09ce773cf6543e8e08228febf7ea6a
SBAPKat <sbapkat@gmail.com>
2020-07-14 22:37:18 +0200

    fixed formating of main.c

--- Sources/main.c
+++ Sources/main.c
@@ -1,26 +1,30 @@
-#include "randomcase.h"
-#include "Init.h"
-#include "Gamelogic.h"
-#include "check_args.h"
-#include "CMD.h"
 #include <stdio.h>
 
-int main(int argc, char** argv){
-	int** GameMatrix = generateMatrix(4,4);	//Creates the game matrix
-	switch (check_args(argc,argv[1])) {
+#include "CMD.h"
+#include "Gamelogic.h"
+#include "Init.h"
+#include "check_args.h"
+#include "randomcase.h"
+
+int main(int argc, char** argv) {
+	int** GameMatrix = generateMatrix(4, 4);  // Creates the game matrix
+	switch (check_args(argc, argv[1])) {
 		case 1:
-			while(mainCMD (GameMatrix)){		//Checks if you still want to play or if you quit
-				GameMatrix=generateMatrix(4,4);	//If you still want to play, reset game matrix
+			while (
+			    mainCMD(GameMatrix)) {  // Checks if you still want
+						    // to play or if you quit
+				GameMatrix = generateMatrix(4, 4);  // If you still want to play, reset
+					    // game matrix
 			}
 			break;
 
 		case 2:
-			//TODO:IMPLEMENT SDL
+			// TODO:IMPLEMENT SDL
 			printf("SDL SELECTED");
 			break;
-		
+
 		case 3:
-			//TODO:IMPLEMENT OPENGL
+			// TODO:IMPLEMENT OPENGL
 			printf("OPENGL SELECTED");
 			break;
 	}
